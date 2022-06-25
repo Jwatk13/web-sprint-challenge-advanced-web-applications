ffe4340738f572cc5bd8d6f01f809e279dc91481
Joseph Watkins <josephxwatkins@gmail.com>
2022-06-25 15:59:11 -0400

    finally got login stuff to work by trial and error. what a pain

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -5,6 +5,9 @@ import LoginForm from './LoginForm'
 import Message from './Message'
 import ArticleForm from './ArticleForm'
 import Spinner from './Spinner'
+import { AuthRoute } from './AuthRoute'
+
+import axios from 'axios'
 
 const articlesUrl = 'http://localhost:9000/api/articles'
 const loginUrl = 'http://localhost:9000/api/login'
@@ -15,6 +18,7 @@ export default function App() {
   const [articles, setArticles] = useState([])
   const [currentArticleId, setCurrentArticleId] = useState()
   const [spinnerOn, setSpinnerOn] = useState(false)
+
 
   // ✨ Research `useNavigate` in React Router v.6
   const navigate = useNavigate()
@@ -31,12 +35,26 @@ export default function App() {
 
   const login = ({ username, password }) => {
     // ✨ implement
+    setMessage("")
+    setSpinnerOn(true);
+    axios.post(loginUrl, {username: username, password: password})
+      .then(res => {
+        console.log(res, "App.js login log")
+        localStorage.setItem("token", res.data.token)
+        setMessage(res.data.message)
+        setSpinnerOn(false)
+        navigate('/articles')
+      })
+      .catch(err => {
+        console.log(err)
+      })
     // We should flush the message state, turn on the spinner
     // and launch a request to the proper endpoint.
     // On success, we should set the token to local storage in a 'token' key,
     // put the server success message in its proper state, and redirect
     // to the Articles screen. Don't forget to turn off the spinner!
   }
+  
 
   const getArticles = () => {
     // ✨ implement
@@ -69,7 +87,7 @@ export default function App() {
     // ✨ fix the JSX: `Spinner`, `Message`, `LoginForm`, `ArticleForm` and `Articles` expect props ❗
     <>
       <Spinner />
-      <Message />
+      <Message message={message}/>
       <button id="logout" onClick={logout}>Logout from app</button>
       <div id="wrapper" style={{ opacity: spinnerOn ? "0.25" : "1" }}> {/* <-- do not change this line */}
         <h1>Advanced Web Applications</h1>
@@ -78,12 +96,12 @@ export default function App() {
           <NavLink id="articlesScreen" to="/articles">Articles</NavLink>
         </nav>
         <Routes>
-          <Route path="/" element={<LoginForm />} />
+          <Route path="/" element={<LoginForm login={login} />} />
           <Route path="articles" element={
-            <>
+            <AuthRoute>
               <ArticleForm />
               <Articles />
-            </>
+            </AuthRoute>
           } />
         </Routes>
         <footer>Bloom Institute of Technology 2022</footer>
--- frontend/components/LoginForm.js
+++ frontend/components/LoginForm.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PT from 'prop-types'
+
 
 const initialFormValues = {
   username: '',
@@ -8,23 +9,35 @@ const initialFormValues = {
 export default function LoginForm(props) {
   const [values, setValues] = useState(initialFormValues)
   // ✨ where are my props? Destructure them here
+  const { login } = props;
+
+  // console.log(values, "LoginForm.js values log")
 
   const onChange = evt => {
     const { id, value } = evt.target
-    setValues({ ...values, [id]: value })
+    setValues({ ...values, [id]: value });
   }
 
   const onSubmit = evt => {
     evt.preventDefault()
     // ✨ implement
+    console.log(login(values))
   }
 
   const isDisabled = () => {
+    if (values.username.trim().length >= 3 && values.password.trim().length >= 8 ) {
+      return false
+    } else {
+      return true
+    }
+    
+  }
+  console.log(isDisabled())
+  console.log(values, "values log")
     // ✨ implement
     // Trimmed username must be >= 3, and
     // trimmed password must be >= 8 for
     // the button to become enabled
-  }
 
   return (
     <form id="loginForm" onSubmit={onSubmit}>
